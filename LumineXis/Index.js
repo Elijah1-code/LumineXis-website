20bc7bc7d792f4ef6af6b189fba4dc2cf6d14c9c
Elijah1-code <136452653+Elijah1-code@users.noreply.github.com>
2024-10-25 08:03:59 +0100

    Add files via upload

---
+++ LumineXis/Index.js
@@ -0,0 +1,46 @@
+ /* Sticky Navbar */
+ window.onscroll = () => {
+    let header = document.querySelector('.header');
+  
+    header.classList.toggle('sticky', window.scrollY > 100);
+  ;}
+/* Welcome */ 
+  var r = confirm("You're welcome to LumineXis. Do well to stay connected with us.");
+  if (r == true) {
+      x = "You pressed OK!";
+  } else {
+      x = "You pressed Cancel!";
+  }
+  
+  /* Swiper */
+  document.addEventListener('DOMContentLoaded', () => {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 50,
+        loop: true,
+        grabCursor: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
+
+
+
+/* Scroll Reveal */
+ScrollReveal({ 
+  reset: true, 
+  distance: '50px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.about-img img, .services-container, .team-box, .testimonial-wrapper, .faq-list, .news-container, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
